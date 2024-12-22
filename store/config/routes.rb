faf86d7df3f93c39fa7c43d75002b9dc6d579c95
tecknovice <tecknovice@gmail.com>
2024-12-22 23:34:22 +0900

    authentication

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :products
 
@@ -11,5 +13,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "products#index"
 end
